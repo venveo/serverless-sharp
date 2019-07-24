7374c45dd2c950c97ab9ba41aaf3b482718ecdde
Ransom <ransom@venveo.com>
2019-07-24 10:39:53 -0400

    Remove demo UI and old gorbage

--- deployment/build-s3-dist.sh
+++ deployment/build-s3-dist.sh
@@ -1,53 +1,29 @@
 echo "------------------------------------------------------------------------------"
 echo "Setup the dist folder"
 echo "------------------------------------------------------------------------------"
-rm -r dist 
+rm -r dist
 mkdir dist
-cd dist
-mkdir demo-ui
-cd ..
 
 echo "------------------------------------------------------------------------------"
 echo "Copy in the template"
 echo "------------------------------------------------------------------------------"
-cp *.template dist/ 
- 
-replace="s/%%BUCKET_NAME%%/$1/g" 
-sed -i '' -e $replace dist/*.template 
- 
-replace="s/%%TEMPLATE_BUCKET%%/$2/g" 
-sed -i '' -e $replace dist/*.template 
- 
-replace="s/%%VERSION%%/$3/g" 
-sed -i '' -e $replace dist/*.template 
- 
-cd ../source 
+cp *.template dist/
+
+replace="s/%%BUCKET_NAME%%/$1/g"
+sed -i '' -e $replace dist/*.template
+
+replace="s/%%TEMPLATE_BUCKET%%/$2/g"
+sed -i '' -e $replace dist/*.template
+
+replace="s/%%VERSION%%/$3/g"
+sed -i '' -e $replace dist/*.template
+
+cd ../source
 
 echo "------------------------------------------------------------------------------"
 echo "Package the image-handler code"
 echo "------------------------------------------------------------------------------"
 cd image-handler
-npm install 
-npm run build 
-cp dist/image-handler.zip ../../deployment/dist/image-handler.zip 
-
-echo "------------------------------------------------------------------------------"
-echo "Package the demo-ui assets"
-echo "------------------------------------------------------------------------------"
-cd ..
-cp -r ./demo-ui/** ../deployment/dist/demo-ui
-
-echo "------------------------------------------------------------------------------"
-echo "Package the custom-resource code"
-echo "------------------------------------------------------------------------------"
-cd custom-resource
-npm install 
-npm run build 
-cp dist/custom-resource.zip ../../deployment/dist/custom-resource.zip 
-
-echo "------------------------------------------------------------------------------"
-echo "Generate the demo-ui manifest document"
-echo "------------------------------------------------------------------------------"
-cd ../../deployment/manifest-generator
 npm install
-node app.js --target ../../source/demo-ui --output ../dist/demo-ui-manifest.json
+npm run build
+cp dist/image-handler.zip ../../deployment/dist/image-handler.zip
