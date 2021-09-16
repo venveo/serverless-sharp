875708c6a68c0ec7d602f1119f742d7b658e61fd
Ransom <ransom@venveo.com>
2021-09-16 08:14:34 -0400

    CDK 2.x isn't ready yet

--- packages/infra-aws/lib/serverless-sharp-stack.ts
+++ packages/infra-aws/lib/serverless-sharp-stack.ts
@@ -1,10 +1,24 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from '@aws-cdk/core';
+const path = require('path');
 
-export class ServerlessSharpStack extends Stack {
-    constructor(scope: Construct, id: string, props?: StackProps) {
+export class ServerlessSharpStack extends cdk.Stack {
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        // const getImageTransformFunction = new aws_lambda.Function(this, 'GetImageTransformFunction', {
+        //     runtime: aws_lambda.Runtime.NODEJS_14_X,
+        //     handler: 'index.handler',
+        //     code: aws_lambda.Code.fromAsset(path.join(__dirname, '../../serverless-sharp/src')),
+        //     tracing: aws_lambda.Tracing.ACTIVE
+        // })
+
+
+        // const getImageTransformApi = new aws_apigatewayv2.CfnApi(this, "ImageTransformApi", {})
+
+        // const proxyIntegration = new aws_apigatewayv2.CfnIntegration()
+
+        // const cloudfrontDistribution = new aws_cloudfront.Distribution(this, 'GetImageTransformDistribution', {
+
+        // })
     }
 }
