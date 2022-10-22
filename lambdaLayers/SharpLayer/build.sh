7ccff195bdd84f2a54e33a3ddadc99fd18d17cd3
Ransom Roberson <ransom@venveo.com>
2022-10-22 16:09:04 -0400

    Various updates

--- lambdaLayers/SharpLayer/build.sh
+++ lambdaLayers/SharpLayer/build.sh
@@ -4,8 +4,24 @@ NODE_VERSION=16
 SHARP_DIRECTORY=sharp-$SHARP_VERSION
 TARBALL=sharp-$SHARP_VERSION-aws-lambda-linux-x64-node-$NODE_VERSION.zip
 
+#LIBVIPS_VERSION_MAJOR=8
+#LIBVIPS_VERSION_MINOR=13
+#LIBVIPS_VERSION_PATCH=2
+#LIBVIPS_VERSION=$LIBVIPS_VERSION_MAJOR.$LIBVIPS_VERSION_MINOR.$LIBVIPS_VERSION_PATCH
+
 # current dir where the build.sh is located
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+## https://docs.aws.amazon.com/en_pv/lambda/latest/dg/configuration-layers.html#configuration-layers-path
+#
+#cd /tmp && \
+#  curl -L https://github.com/libvips/libvips/releases/download/v$LIBVIPS_VERSION/vips-$LIBVIPS_VERSION.tar.gz > vips-$LIBVIPS_VERSION.tar.gz && \
+#  tar xf vips-$LIBVIPS_VERSION.tar.gz && \
+#  cd vips-$LIBVIPS_VERSION && \
+#  ./configure --enable-debug=no --without-python --without-magick && \
+#  make && make install && ldconfig
+#
+#pkg-config --modversion vips-cpp
 
 cd $DIR
 
@@ -13,6 +29,7 @@ cd $DIR
 
 # NPM install sharp
 npm install --production --prefix ./nodejs sharp@$SHARP_VERSION
+#npm install  --arch=x64 --platform=linux --libc=glibc sharp --production --prefix ./nodejs sharp@$SHARP_VERSION
 
 # Delete everything except for the sharp directory
 #find ./nodejs/node_modules -mindepth 1 ! -regex '^./nodejs/node_modules/sharp\(/.*\)?' -delete
