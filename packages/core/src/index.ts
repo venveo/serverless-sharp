daec0f8102a56f4c78ed41238bd1fb3b23659122
Ransom Roberson <ransom@venveo.com>
2023-01-15 13:23:50 -0500

    Switch from nx/lerna to turborepo

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,13 @@
-export * as ImageRequest from './ImageRequest'
-export * as ImageHandler from './ImageHandler'
+import { GenericHttpInvocationEvent } from './types/common';
+import { fromPromise, ResultAsync } from 'neverthrow';
+import ImageRequest from './ImageRequest';
+
+export * as ImageRequest from './ImageRequest';
+export * as ImageHandler from './ImageHandler';
+
+export const createImageRequest = (config: GenericHttpInvocationEvent): ResultAsync<ImageRequest, string> => {
+  const request = new ImageRequest(config);
+  return fromPromise(request.process(), () => {
+    return 'Failed to create image request'
+  });
+};
